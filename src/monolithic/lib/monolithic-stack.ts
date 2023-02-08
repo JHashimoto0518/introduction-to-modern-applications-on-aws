f017c53a4e799aaa28486b9b23d59faa76c34331
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2023-02-09 05:56:48 +0900

    added vpc

--- src/monolithic/lib/monolithic-stack.ts
+++ src/monolithic/lib/monolithic-stack.ts
@@ -1,9 +1,28 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class MonolithicStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, 'vpc', {
+      vpcName: "sbs-dev-vpc",
+      ipAddresses: ec2.IpAddresses.cidr('172.16.0.0/16'),
+      natGateways: 1,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'pub',
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: 'pri',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+        }
+      ],
+    });
   }
 }
