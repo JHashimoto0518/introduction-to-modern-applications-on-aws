69bf3a15b71e725d2762ce614904ed47e0ce00d8
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2023-04-21 05:42:17 +0900

    refactor: changed naming rule

--- src/monolithic/cdk/lib/monolithic-stack.ts
+++ src/monolithic/cdk/lib/monolithic-stack.ts
@@ -1,9 +1,9 @@
-import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import * as rds from "aws-cdk-lib/aws-rds";
+import * as rds from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
 
 export class MonolithicStack extends Stack {
@@ -11,36 +11,38 @@ export class MonolithicStack extends Stack {
     super(scope, id, props);
 
     // vpc
-    const vpc = new ec2.Vpc(this, 'vpc', {
-      vpcName: "sbs-dev-vpc",
+    const vpc = new ec2.Vpc(this, 'BookStoreVpc', {
+      vpcName: 'bookstore-vpc',
       ipAddresses: ec2.IpAddresses.cidr('172.16.0.0/16'),
       natGateways: 0,
       maxAzs: 2,
       subnetConfiguration: [
         {
           cidrMask: 24,
-          name: 'public',
+          name: 'Public',
           subnetType: ec2.SubnetType.PUBLIC
         },
         {
           cidrMask: 24,
-          name: 'private',
+          name: 'Private',
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED
         }
       ],
     });
-    // add private endpoints for ssm
-    vpc.addInterfaceEndpoint('ssm', {
+
+    // add private endpoints for session manager
+    vpc.addInterfaceEndpoint('SsmEndpoint', {
       service: ec2.InterfaceVpcEndpointAwsService.SSM,
     });
-    vpc.addInterfaceEndpoint('ssmmessages', {
+    vpc.addInterfaceEndpoint('SsmMessagesEndpoint', {
       service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
     });
-    vpc.addInterfaceEndpoint('ec2messages', {
+    vpc.addInterfaceEndpoint('Ec2MessagesEndpoint', {
       service: ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES,
     });
+
     // add private endpoint for Amazon Linux repository on s3
-    vpc.addGatewayEndpoint('s3', {
+    vpc.addGatewayEndpoint('S3Endpoint', {
       service: ec2.GatewayVpcEndpointAwsService.S3,
       subnets: [
         { subnetType: ec2.SubnetType.PRIVATE_ISOLATED }
@@ -48,68 +50,71 @@ export class MonolithicStack extends Stack {
     });
 
     //
-    // Security Groups
+    // security group
     //
-    const albSg = new ec2.SecurityGroup(this, "alb-sg", {
+    const albSg = new ec2.SecurityGroup(this, 'AlbSg', {
       vpc,
       allowAllOutbound: true,
-      description: "security group for alb"
+      description: 'security group for alb'
     })
-    albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "allow http traffic from anyone")
+    albSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'allow http traffic from anywhere')
 
-    const webServerSg = new ec2.SecurityGroup(this, "web-server-sg", {
+    const ec2Sg = new ec2.SecurityGroup(this, 'AppEc2Sg', {
       vpc,
       allowAllOutbound: true,
-      description: "security group for a web server"
+      description: 'security group for application servers'
     })
-    webServerSg.connections.allowFrom(albSg, ec2.Port.tcp(80), 'allow http traffic from alb')
+    ec2Sg.connections.allowFrom(albSg, ec2.Port.tcp(80), 'allow http traffic from alb')
 
     //
-    // web server
+    // role
     //
-    const webServerRole = new iam.Role(this, 'web-server-role', {
+    const ec2Role = new iam.Role(this, 'AppRole', {
       assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName(
-          "AmazonSSMManagedInstanceCore"    // for managed by SSM
+          'AmazonSSMManagedInstanceCore'
         ),
       ],
-      description: 'role for web server ec2',
+      description: 'role for application servers',
     });
 
+    //
+    // application server
+    //
     const userData = ec2.UserData.forLinux({
-      shebang: "#!/bin/bash",
+      shebang: '#!/bin/bash',
     })
     userData.addCommands(
       // setup httpd
-      "yum update -y",
-      "yum install -y httpd",
-      "systemctl start httpd",
-      "systemctl enable httpd",
+      'yum update -y',
+      'yum install -y httpd',
+      'systemctl start httpd',
+      'systemctl enable httpd',
       "sh -c 'echo \"This is a sample bookstore website.\" > /var/www/html/index.html'",
     )
 
-    const launchTemplate = new ec2.LaunchTemplate(this, 'web-server-lt', {
-      launchTemplateName: "sbs-dev-lt-web",
-      instanceType: new ec2.InstanceType("t2.micro"),
+    const launchTmpl = new ec2.LaunchTemplate(this, 'AppLaunchTmpl', {
+      launchTemplateName: 'app-launch-tmpl',
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: ec2.MachineImage.latestAmazonLinux({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
       }),
       blockDevices: [
         {
-          deviceName: "/dev/xvda",
+          deviceName: '/dev/xvda',
           volume: autoscaling.BlockDeviceVolume.ebs(8, {
             volumeType: autoscaling.EbsDeviceVolumeType.GP3,
           }),
         },
       ],
-      securityGroup: webServerSg,
-      role: webServerRole,
+      securityGroup: ec2Sg,
+      role: ec2Role,
       userData,
     });
 
-    const webAsg = new autoscaling.AutoScalingGroup(this, 'asg-web', {
-      launchTemplate,
+    const asGrpWeb = new autoscaling.AutoScalingGroup(this, 'AppAutoScalingGrp', {
+      launchTemplate: launchTmpl,
       vpc,
       vpcSubnets: vpc.selectSubnets({
         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
@@ -117,14 +122,14 @@ export class MonolithicStack extends Stack {
       desiredCapacity: 1,
       maxCapacity: 3,
     })
-    webAsg.scaleOnCpuUtilization('CPU50Percent', {
+    asGrpWeb.scaleOnCpuUtilization('Cpu50Percent', {
       targetUtilizationPercent: 50
     });
 
     //
-    // ALB
+    // alb
     //
-    const alb = new elbv2.ApplicationLoadBalancer(this, "alb", {
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'Alb', {
       internetFacing: true,
       vpc,
       vpcSubnets: {
@@ -133,50 +138,43 @@ export class MonolithicStack extends Stack {
       securityGroup: albSg
     })
 
-    const albListener = alb.addListener("AlbHttpListener", {
+    const listener = alb.addListener('HttpListener', {
       port: 80,
       protocol: elbv2.ApplicationProtocol.HTTP
     })
-    albListener.addTargets("WebServerTarget", {
-      targets: [webAsg],
+    listener.addTargets('AppTarget', {
+      targets: [asGrpWeb],
       port: 80
     })
 
     //
-    // RDS
+    // rds
     //
     const engine = rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0_31 });
-    const parameterGroup = new rds.ParameterGroup(this, "BookStoreParamGrp", {
+    const paramGrp = new rds.ParameterGroup(this, 'BookStoreParamGrp', {
       engine,
-      description: "for BookStoreDB"
+      description: 'for bookstore'
     })
-    const subnetGroup = new rds.SubnetGroup(this, 'BookStoreSubnetGrp', {
-      description: "for BookStoreDB",
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-      },
-    });
-    const optionGroup = new rds.OptionGroup(this, 'BookStoreOptGrp', {
+    const optGrp = new rds.OptionGroup(this, 'BookStoreOptGrp', {
       engine,
       configurations: [],
-      description: "for BookStoreDB"
+      description: 'for bookstore'
     })
 
-    new rds.DatabaseInstance(this, "BookStoreDB", {
-      engine: engine,
-      vpc: vpc,
+    const dbInstance = new rds.DatabaseInstance(this, 'BookStoreDbInstance', {
+      engine,
+      vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
       vpcSubnets: {
         subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
       },
-      databaseName: "bookstore",
-      subnetGroup,
-      parameterGroup,
-      optionGroup,
+      databaseName: 'bookstore',
+      parameterGroup: paramGrp,
+      optionGroup: optGrp,
       multiAz: true,
       deleteAutomatedBackups: true,
-      removalPolicy: RemovalPolicy.DESTROY    // To avoid OptionGroup deletion error, do not leave any snapshots
-    }).connections.allowDefaultPortFrom(webServerSg);
+      removalPolicy: RemovalPolicy.DESTROY    // to avoid OptionGroup deletion error, do not leave any snapshots
+    });
+    dbInstance.connections.allowDefaultPortFrom(ec2Sg);
   }
 }
